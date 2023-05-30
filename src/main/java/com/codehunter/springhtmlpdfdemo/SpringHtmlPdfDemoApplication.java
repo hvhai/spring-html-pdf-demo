ef9138293d8b936ccbda5b6bb953bdec4b69f4ac
hvhai <hunterx6323@gmail.com>
2023-05-30 15:24:38 +0700

    init project

---
+++ src/main/java/com/codehunter/springhtmlpdfdemo/SpringHtmlPdfDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codehunter.springhtmlpdfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHtmlPdfDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHtmlPdfDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codehunter/springhtmlpdfdemo/SpringHtmlPdfDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehunter.springhtmlpdfdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHtmlPdfDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
